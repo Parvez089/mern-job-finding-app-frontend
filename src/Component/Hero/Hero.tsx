155bf3b34e821e5d8caefef939286d07d975b091
parvez <mhparvez089@gmail.com>
2025-10-19 20:56:07 +0600

    02: Hero fix

--- src/Component/Hero/Hero.tsx
+++ src/Component/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import { Button } from "antd";
 import SearchPage from "./Search";
 import { ArrowRightOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -16,7 +17,10 @@ const Hero = () => {
           <p className="text">Create an account or sign in to see your personalized job recommendations.</p>
         </div>
         <div className="flex justify-center ">
-        <Button className=" !items-center py-4 !bg-[var(--bg-color)] !text-white !font-semibold">Get Started <ArrowRightOutlined className="!font-semibold "/> </Button>
+          <Link href={"/auth"}>
+ <Button className=" !items-center !text-xl py-4 w-48 !h-12 !bg-[var(--bg-color)] !text-white !font-semibold">Get Started <ArrowRightOutlined className="!font-semibold "/> </Button>
+          </Link>
+       
         </div>
         
           
